fe536c3eebf1345229eb55d87a9f40ee776d19cc
Tushar Soni <tushargsoni@outlook.com>
2025-06-24 13:25:59 +0530

    added react router so / renders Landingpage with simple Cooming soon page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,15 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import LandingPage from './components/LandingPage';
+
+
 function App() {
   return (
-    <>
-      <h1 className="text-3xl font-bold uppercase">Hello PantryPal</h1>
-    </>
-  );
+    <Router>
+      <Routes>
+        <Route path='/' element={<LandingPage />} />
+      </Routes>
+    </Router>
+  )
 }
 
 export default App;
--- client/src/components/LandingPage.jsx
+++ client/src/components/LandingPage.jsx
@@ -4,7 +4,11 @@ import Footer from "./Footer";
 function LandingPage() {
   return (
     <>
-      <div>LandingPage</div>
+      <div className="dark:bg-gray-900 text-gray-400 min-h-screen mx-auto flex justify-center items-center">
+        <div>
+            <h1 className="text-xl font-bold">Cooming Soon...</h1>
+        </div>
+      </div>
       <Footer />
     </>
   );
