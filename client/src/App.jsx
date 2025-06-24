629a553393176c6f11f4f9445a754a9f323c6cb6
Tushar Soni <tushargsoni@outlook.com>
2025-06-24 13:01:37 +0530

    created a footer component and landingpage component and added footer in it made the footer simple yet responsive.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,9 @@
 function App() {
-  return <h1 className="text-3xl font-bold uppercase">Hello PantryPal</h1>;
+  return (
+    <>
+      <h1 className="text-3xl font-bold uppercase">Hello PantryPal</h1>
+    </>
+  );
 }
 
 export default App;
---
+++ client/src/components/LandingPage.jsx
@@ -0,0 +1,13 @@
+
+import Footer from "./Footer";
+
+function LandingPage() {
+  return (
+    <>
+      <div>LandingPage</div>
+      <Footer />
+    </>
+  );
+}
+
+export default LandingPage;
