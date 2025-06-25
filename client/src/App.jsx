8a1aefd1d5d419cbcfa992bb67a28094a9c9b3ce
abutler911 <abutler911@gmail.com>
2025-06-24 20:34:12 -0600

    fix(client): single BrowserRouter + correct Link import

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,14 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import LandingPage from './components/LandingPage';
-import Login from './components/Login';
-
+import { Routes, Route } from "react-router-dom";
+import LandingPage from "./components/LandingPage";
+import Login from "./components/Login";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path='/' element={<LandingPage />} />
-        <Route path='/login' element={<Login />} />
-      </Routes>
-    </Router>
-  )
+    <Routes>
+      <Route path="/" element={<LandingPage />} />
+      <Route path="/login" element={<Login />} />
+    </Routes>
+  );
 }
 
 export default App;
